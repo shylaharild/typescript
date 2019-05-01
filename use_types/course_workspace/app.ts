dcd2ce4cddbfb7e93edaa70b147bb53c4f68c364
Sri Laguduva <sri.laguduva@deepcrawl.com>
2019-05-01 14:34:23 +0100

    type alias

--- use_types/course_workspace/app.ts
+++ use_types/course_workspace/app.ts
@@ -97,3 +97,13 @@ let complex: {data: number[], output: (all: boolean) => number[]} = {
     }
 };
 // complex = {};
+
+// type alias
+type Complex = {data: number[], output: (all: boolean) => number[]};
+let complex2: Complex = {
+    data: [100, 3.99, 10],
+
+    output: function (all: boolean): number[] {
+        return this.data
+    }
+};
