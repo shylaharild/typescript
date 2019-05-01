209f770631780d5065195204a30f54f08b1d898c
Sri Laguduva <sri.laguduva@deepcrawl.com>
2019-05-01 13:50:40 +0100

    missed ts file

---
+++ use_types/course_workspace/app.ts
@@ -0,0 +1,29 @@
+console.log("It works!");
+
+// strings
+let myName = 'Sri';
+// myName = 25;
+
+// numbers
+let myAge = 27
+// myAge = 'Sri'
+
+// boolean
+let hasHobbies = true;
+// hasHobbies = 1;
+// hasHobbies = "false";
+
+// assign types
+let myRealAge: number = 30;
+// myRealAge = '30';
+
+// array (it is an object)
+let hobbies_1 = ["Cooking", "games"];
+console.log(hobbies_1)
+// hobbies_1 = [100, 200];
+let hobbies: any[] = ["Cooking", "games"];
+hobbies = [100];
+hobbies = [true];
+
+// tuples
+let add: [string, number] = ["Malibu Point", 10880];
