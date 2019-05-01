2b1b53752c360c1d52f9b5014b77d8c871865c75
Sri Laguduva <sri.laguduva@deepcrawl.com>
2019-05-01 17:04:48 +0100

    let and const

---
+++ typescript_es6/app.ts
@@ -0,0 +1,11 @@
+console.log("It works!");
+
+// let & const
+let variable = "Test";
+console.log(variable);
+variable = "Another value";
+console.log(variable)
+
+const maxLevels = 100; // the value assigned to constant cannot be changed
+console.log(maxLevels);
+// maxLevels = 101;
